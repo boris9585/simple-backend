73e9860a73fbd510641389b1a6f3ec379be41e8f
Boris Ivanov <boris9585@outlook.com>
2023-03-24 08:04:23 +0300

    ChatGPT module imported

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+import { ChatGPTAPI } from 'chatgpt';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3002;
+const OPENAI_API_KEY = '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';
 
 app.get('/', (req, res) => res.send('success'));
 
