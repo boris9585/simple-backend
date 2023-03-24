928e8c6b71e26f5c55d6cc6bbc527593c3fa5659
Boris Ivanov <boris9585@outlook.com>
2023-03-24 08:16:27 +0300

    ChatGPT api added

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const app = express();
 const port = process.env.PORT || 3002;
 const OPENAI_API_KEY = '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';
 
-app.get('/', (req, res) => res.send('success'));
+app.get('/', (req, res) => res.send('still success'));
+app.post('/find', async (req, res) => {
+    const api = new ChatGPTAPI({
+        apiKey: OPENAI_API_KEY
+    });
+
+    try {
+        const result = await api.sendMessage(req.body.text);
+        res.send(result.text);
+    } catch (e) {
+        console.log(e);
+    }
+})
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
